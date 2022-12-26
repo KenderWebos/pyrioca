b2c47c29edff1a85291f70f225108481bef44a26
Kevin Campos Venegas <kenderman.8@gmail.com>
2022-12-25 21:51:35 -0300

    maris commit

--- pyrioca.py
+++ pyrioca.py
@@ -4,8 +4,39 @@ import os
 
 cantJugadores = 2
 cantCartasInicial = 6
+    
+mazo = []
+#1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "q", "k", "joker"
 
-mazo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "q", "k", "joker"]
+for j in range(10):
+    mazo.append(str("p_"+ str(j+1)))
+mazo.append("p_j")
+mazo.append("p_q")
+mazo.append("p_k")
+
+for j in range(10):
+    mazo.append(str("c_"+ str(j+1)))
+mazo.append("c_j")
+mazo.append("c_q")
+mazo.append("c_k")
+
+for j in range(10):
+    mazo.append(str("t_"+ str(j+1)))
+mazo.append("t_j")
+mazo.append("t_q")
+mazo.append("t_k")
+
+for j in range(10):
+    mazo.append(str("d_"+ str(j+1)))
+mazo.append("d_j")
+mazo.append("d_q")
+mazo.append("d_k")
+
+# for e in mazo:
+#     mazo.append( e )
+
+print(mazo)
+
 mazoDescarte = []
 random.shuffle(mazo)
 
@@ -54,5 +85,4 @@ def printData(player):
 printUI(0)
 repartirCartas()
 printData(0)
-
 
